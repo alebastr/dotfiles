e2af75146f3336ce4d1e75b6b8cd7078b68c0d4c
Aleksei Bavshin <alebastr89@gmail.com>
2019-03-16 01:17:36 -0700

    [sway] add initial configuration

---
+++ sway/autorun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+run() {
+	local delay="0.1s"
+	if [[ "$1" =~ ^[0-9\.]+[smhd]?$ ]]; then
+		delay=$1; shift
+	fi
+	if ! pidof $1 >/dev/null; then
+#        echo "starting \"${@}\" in ${delay}";
+		(sleep $delay && $@)&
+	fi
+}
+
+# ssh-agent
+run gpg-connect-agent /bye
+# notifications
+run mako
+# xembed tray -> SNI translation
+run xembedsniproxy
+
+# delay start until waybar is ready
+run 1 seafile-applet
