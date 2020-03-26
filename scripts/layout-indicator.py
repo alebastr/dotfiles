f524f57c3380e87a2a6a5caf82e99f7da2e2f28e
Aleksei Bavshin <alebastr89@gmail.com>
2020-03-25 23:09:36 -0700

    [waybar] add active container layout indicator

---
+++ scripts/layout-indicator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import i3ipc
+import json
+import sys
+
+short = {
+    "splitv": "V",
+    "splith": "H",
+    "tabbed": "T",
+    "stacked": "S",
+}
+last = ""
+
+
+def write(text, tooltip=""):
+    output = {"text": text, "tooltip": tooltip, "class": tooltip}
+    sys.stdout.write(json.dumps(output) + "\n")
+    sys.stdout.flush()
+
+
+def on_event(self, _):
+    global last
+    layout = i3.get_tree().find_focused().parent.layout
+    if layout != last:
+        write(short.get(layout, " "), layout)
+    last = layout
+
+
+i3 = i3ipc.Connection()
+
+# Subscribe to events
+i3.on("window::focus", on_event)
+i3.on("binding", on_event)
+
+write("?")
+# Start the main loop and wait for events to come in.
+i3.main()
