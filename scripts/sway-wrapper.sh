f092f1c5271e73a1213383d967163e9ac2c101e0
Aleksei Bavshin <alebastr89@gmail.com>
2021-04-21 21:16:46 -0700

    fix: remove files obsoleted by sway-systemd[1]
    [1]: https://github.com/alebastr/sway-systemd

--- scripts/sway-wrapper.sh
+++ scripts/sway-wrapper.sh
@@ -9,19 +9,4 @@ export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 export XDG_SESSION_TYPE=wayland
 
-# redirect errors to a file in user's home directory if we can
-#errfile="$HOME/.xsession-errors"
-#if ( umask 077 && cp /dev/null "$errfile" 2> /dev/null ); then
-#    chmod 600 "$errfile"
-#    [ -x /sbin/restorecon ] && /sbin/restorecon $errfile
-#    exec > "$errfile" 2>&1
-#else
-#    errfile=$(mktemp -q /tmp/xses-$USER.XXXXXX)
-#    if [ $? -eq 0 ]; then
-#        exec > "$errfile" 2>&1
-#    fi
-#fi
-
-/bin/systemd-cat "/usr/bin/$(basename "$0")" "$@"
-# stop systemd user services
-/usr/bin/systemctl --user stop graphical-session.target graphical-session-pre.target
+exec /bin/systemd-cat "/usr/bin/$(basename "$0")" "$@"
